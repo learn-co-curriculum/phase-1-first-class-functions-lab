fb838d32eaa3f5578dadb4660f749c7a167fec53
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-07-20 17:12:35 -0600

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+let returnFirstTwoDrivers = (driversArr1) => {
+    const copyDriversArray1 = [...driversArr1]
+    return copyDriversArray1.slice(0, 2)
+}
+
+let returnLastTwoDrivers = (lastTwoArray) => {
+    const driversArray = [...lastTwoArray]
+    return driversArray.slice(-2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer){
+    return fare => fare * integer;
+}
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(driversArray, firstTwoDrivers) {
+    return firstTwoDrivers(driversArray);
+}
