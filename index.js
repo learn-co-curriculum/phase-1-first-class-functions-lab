d3de93dd081b6b8639a6ad91a209771f8654e351
Sam Chappel <m.samchappel@gmail.com>
2023-01-11 14:43:10 -0800

    7 passing, 6 tests remaining

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return [drivers[0], drivers[1]]
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return [drivers[drivers.length-2], drivers[drivers.length-1]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(riders) {
+    return function(fare) {
+        return Math.abs(fare * riders)
+    }
+}
